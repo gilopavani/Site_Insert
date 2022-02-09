eb9550e6e5a0e64d40a497db4b4ace77633ed1ac
Angelo Pavani Neto <gilopavani@gmail.com>
2022-02-08 21:57:08 -0300

    config server apache

--- conecta.php
+++ conecta.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$tabela = "lista-os";
-$usuario = "root";
-$senha = "";
+$host = "us-cdbr-east-05.cleardb.net";
+$tabela = "heroku_3166029c31857d1";
+$usuario = "b7ee025ae16aa3";
+$senha = "3a697031";
 // Create connection
 $link = mysqli_connect ($host, $usuario,$senha, $tabela);
 
