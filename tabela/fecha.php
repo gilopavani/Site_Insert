0e0f5cf2e8c973b1ae13b7e39b8ab5a5e52f97b0
iurireiss <99371239+iurireiss@users.noreply.github.com>
2022-02-21 20:47:56 -0300

    ultimo

--- tabela/fecha.php
+++ tabela/fecha.php
@@ -2,11 +2,16 @@
 include('../conecta.php');
 
 $fecha = $_POST['id'] ;
+$nome = $_POST['tecnico'] ;
 
+echo $fecha;
+echo "<br>";
+echo $nome;
 
-$sql = "UPDATE `os` SET `Fechado` = '0' WHERE `os`.`id` = $fecha";
+$sql = "UPDATE `os` SET `Fechado` = '0', `tecnico` = '$nome' WHERE (id = '$fecha') ";
 
 $result = mysqli_query($link, $sql);
 header('Location: index.php');
+
 
 ?>
